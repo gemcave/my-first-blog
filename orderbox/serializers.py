3118a6406a7abb61719280bdf234a814fa61e9ee
gemcave <stevendrosse@gmail.com>
2015-12-15 12:46:56 +0600

    FIX#2: serializers,paths was fixed

--- orderbox/serializers.py
+++ orderbox/serializers.py
@@ -1,5 +1,6 @@
+from orderbox.models import Order
 from rest_framework import serializers
-from models import Order
+
 
 class OrderSerializer(serializers.ModelSerializer):
 	class Meta:
--- orderbox/views.py
+++ orderbox/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from rest_framework import generics
-from serializers import OrderSerializer
-from models import Order
+from orderbox.serializers import OrderSerializer
+from orderbox.models import Order
 
 # Create your views here.
-class  OrderList(generics.ListCreateAPIView):
+class OrderList(generics.ListCreateAPIView):
 	queryset = Order.objects.all()
 	serializer_class = OrderSerializer
 
 
-class ZipDetail(generics.RetrieveUpdateDestroyAPIView):
+class OrderDetail(generics.RetrieveUpdateDestroyAPIView):
 	queryset = Order.objects.all()
 	serializer_class = OrderSerializer
